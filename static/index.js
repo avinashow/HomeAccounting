e62c796d5ce035fa34d5188a1a41b501554610ab
Pavan Mellamputi <theone.pa1@gmail.com>
2019-07-15 23:53:29 -0700

    Implemented the endpoint for overview to return mock data

--- static/index.js
+++ static/index.js
@@ -1,6 +1,23 @@
-var app = new Vue({
+const List = Vue.Component('List',{
+  template: `
+    <h1> welcome to home page</h1>
+  `,
+  data: function() {
+    return {
+      message: 'welcome to vue'
+    }
+  },
+})
+const router = new VueRouter({
+	routes:[
+		{
+			path:'/',
+			component:List,
+		},
+	]
+});
+
+const app = new Vue({
   el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+  router,
 });
