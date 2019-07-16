1209f63cdb27aec0e2c5cd59809534257bb5e5a8
Pavan Mellamputi <theone.pa1@gmail.com>
2019-07-15 23:53:29 -0700

    Implemented the endpoint for overview to return mock data

--- main.py
+++ main.py
@@ -3,30 +3,33 @@ from endpoints import message_types
 from endpoints import messages
 from endpoints import remote
 
-class EchoRequest(messages.Message):
-    message = messages.StringField(1)
+class OverviewResponseItem(messages.Message):
+    name = messages.StringField(1)
+    outstanding_principal = messages.FloatField(2)
+    outstanding_interest = messages.FloatField(3)
+    paid_interest = messages.FloatField(4)
+    date = messages.IntegerField(5)
+    last_transaction_date = messages.IntegerField(6)
 
+class OverviewResponse(messages.Message):
+    items = messages.MessageField(OverviewResponseItem, 1, repeated=True)
 
-class EchoResponse(messages.Message):
-    """A proto Message that contains a simple string field."""
-    message = messages.StringField(1)
-
-
-ECHO_RESOURCE = endpoints.ResourceContainer(
-    EchoRequest,
-    n=messages.IntegerField(2, default=1))
-
-@endpoints.api(name='echo', version='v1')
+@endpoints.api(name='homac', version='v1')
 class EchoApi(remote.Service):
 
     @endpoints.method(
-        ECHO_RESOURCE,
-        EchoResponse,
-        path='echo',
-        http_method='GET',
-        name='echo')
-    def echo(self, request):
-        output_message = ' '.join([request.message] * request.n)
-        return EchoResponse(message=output_message)
+        message_types.VoidMessage,
+        OverviewResponse,
+        path='overview',
+        http_method='GET')
+    def overview(self, request):
+        mock_item_1 = OverviewResponseItem(name='Pavan', outstanding_principal=100000.0,
+                                           outstanding_interest=5000.0, paid_interest=1000.0, date=1562259702,
+                                           last_transaction_date=1562299702)
+        mock_item_2 = OverviewResponseItem(name='Avinash', outstanding_principal=200000.0,
+                                           outstanding_interest=15000.0, paid_interest=4000.0, date=1563059702,
+                                           last_transaction_date=1563159702)
+        items = [mock_item_1, mock_item_2]
+        return OverviewResponse(items=items)
 
 api = endpoints.api_server([EchoApi])
