fec3a3b0e2c47aeb4213e54f07afa434f1bce0a2
Pavan Mellamputi <theone.pa1@gmail.com>
2019-07-21 15:56:20 -0700

    Add a new property to Overview response

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from endpoints import message_types
 from endpoints import messages
 from endpoints import remote
 
+
 class OverviewResponseItem(messages.Message):
     name = messages.StringField(1)
     outstanding_principal = messages.FloatField(2)
     outstanding_interest = messages.FloatField(3)
     paid_interest = messages.FloatField(4)
-    date = messages.IntegerField(5)
-    last_transaction_date = messages.IntegerField(6)
+    paid_principal = messages.FloatField(5)
+    date = messages.IntegerField(6)
+    last_transaction_date = messages.IntegerField(7)
+
 
 class OverviewResponse(messages.Message):
     items = messages.MessageField(OverviewResponseItem, 1, repeated=True)
+
 
 @endpoints.api(name='homac', version='v1')
 class EchoApi(remote.Service):
@@ -24,11 +28,11 @@ class EchoApi(remote.Service):
         http_method='GET')
     def overview(self, request):
         mock_item_1 = OverviewResponseItem(name='Pavan', outstanding_principal=100000.0,
-                                           outstanding_interest=5000.0, paid_interest=1000.0, date=1562259702,
-                                           last_transaction_date=1562299702)
+                                           outstanding_interest=5000.0, paid_interest=1000.0, paid_principal=0.0,
+                                           date=1562259702, last_transaction_date=1562299702)
         mock_item_2 = OverviewResponseItem(name='Avinash', outstanding_principal=200000.0,
-                                           outstanding_interest=15000.0, paid_interest=4000.0, date=1563059702,
-                                           last_transaction_date=1563159702)
+                                           outstanding_interest=15000.0, paid_interest=4000.0, paid_principal=25000.0,
+                                           date=1563059702, last_transaction_date=1563159702)
         items = [mock_item_1, mock_item_2]
         return OverviewResponse(items=items)
 
