77c39090e11ade3fc88ea5b53c8b6be158301b68
avinashow <avinashow@gmail.com>
2019-07-21 19:01:24 -0700

    overview page - currency format

--- static/index.js
+++ static/index.js
@@ -41,7 +41,7 @@ Vue.component('manage-transactions', {
     calculateTotalOutstandingInterest() {
       let total = 0;
       this.summaryItems.forEach(function(item) {
-        total += parseInt(item.paid_interest);
+        total += parseInt(item.outstanding_interest);
       });
       return this.formatCurrency(total);
     },
