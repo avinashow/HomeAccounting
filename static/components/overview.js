135c40ba73a2444166710ffc75b023f7984c10d4
avinashow <avinashow@gmail.com>
2019-07-28 23:14:46 -0700

    overview css change

--- static/components/overview.js
+++ static/components/overview.js
@@ -2,18 +2,72 @@ const summary = Vue.component('manage-transactions', {
     template:`
     <div>
         <h1>{{title}}</h1>
-        <span>
-            <strong>Total outstanding principal:</strong> {{ calculateTotalOutstandingPrincipal() | formatCurrency }}
-        </span>
-        <span>
-            <strong>Total paid principal:</strong> {{ calculateTotalPaidPrincipal() | formatCurrency }}
-        </span>
-        <span>
-            <strong>Total outstanding interest:</strong> {{ calculateTotalOutstandingInterest() | formatCurrency }}
-        </span>
-        <span>
-            <strong>Total paid interest:</strong> {{ calculateTotalPaidInterest() | formatCurrency }}
-        </span>
+        <div class="row">
+          <div class="col-md-3 col-xl-4">
+            <div class="card mb-3 widget-content">
+              <div class="widget-content-wrapper">
+                <div class="widget-content-left">
+                  <div class="widget-heading">
+                    Total outstanding principal
+                  </div>
+                </div>
+                <div class="widget-content-right">
+                  <div class="widget-numbers">
+                    <span>{{ calculateTotalOutstandingPrincipal() | formatCurrency }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-xl-4">
+            <div class="card mb-3 widget-content">
+              <div class="widget-content-wrapper">
+                <div class="widget-content-left">
+                  <div class="widget-heading">
+                    Total paid principal
+                  </div>
+                </div>
+                <div class="widget-content-right">
+                  <div class="widget-numbers">
+                    <span>{{ calculateTotalPaidPrincipal() | formatCurrency }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-xl-4">
+            <div class="card mb-3 widget-content">
+              <div class="widget-content-wrapper">
+                <div class="widget-content-left">
+                  <div class="widget-heading">
+                    Total outstanding interest
+                  </div>
+                </div>
+                <div class="widget-content-right">
+                  <div class="widget-numbers">
+                    <span>{{ calculateTotalOutstandingInterest() | formatCurrency }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-xl-4">
+            <div class="card mb-3 widget-content">
+              <div class="widget-content-wrapper">
+                <div class="widget-content-left">
+                  <div class="widget-heading">
+                    Total paid interest
+                  </div>
+                </div>
+                <div class="widget-content-right">
+                  <div class="widget-numbers">
+                    <span>{{ calculateTotalPaidInterest() | formatCurrency }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <table class="table">
             <thead>
                 <tr>
