acf88673aa2241b0057164857137ee8bce38378d
avinashow <avinashow@gmail.com>
2019-07-31 23:48:13 -0700

    included edit, delete buttons in transaction ui

--- static/components/transactions.js
+++ static/components/transactions.js
@@ -1,40 +1,42 @@
 export const TransactionPage = Vue.component('view-transactions', {
     template: `
-    <div>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Payment type</th>
-                    <th scope="col">Transaction date</th>
-                    <th scope="col">Amount</th>
-                    <th scope="col">Options</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="transaction in transactions">
-                    <th scope="row">{{transaction.transaction_id}}</th>
-                    <td>{{transaction.borrower_name | formatLabel}}</td>
-                    <td>{{transaction.type}}</td>
-                    <td>{{transaction.payment_type}}</td>
-                    <td>{{transaction.transaction_date | formatDate}}</td>
-                    <td>{{transaction.amount | formatCurrency}}</td>
-                    <td>
-                        <button type="button" class="btn btn-primary">
-                            <i class="fa fa-eye"></i>
-                        </button>
-                        <router-link :to="{ name:'editTransaction', params:{data: transaction} }" >
-                            <i class="fa fa-edit"></i>
-                        </router-link>
-                        <button type="button" class="btn btn-danger">
-                            <i class="fa fa-trash-o"></i>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="row">
+        <div class="col grid-margin">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Type</th>
+                                <th scope="col">Payment type</th>
+                                <th scope="col">Transaction date</th>
+                                <th scope="col">Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="transaction in transactions">
+                                <th scope="row">{{transaction.transaction_id}}</th>
+                                <td>{{transaction.borrower_name | formatLabel}}</td>
+                                <td>{{transaction.type}}</td>
+                                <td>{{transaction.payment_type}}</td>
+                                <td>{{transaction.transaction_date | formatDate}}</td>
+                                <td>{{transaction.amount | formatCurrency}}</td>
+                                <td>
+                                    <router-link :to="{ name:'editTransaction', params:{data: transaction} }" class="btn btn-outline-success btn-sm" >
+                                        Edit
+                                    </router-link>
+                                    <button type="button" class="btn btn-outline-danger btn-sm">
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
     `,
     data: function() {
