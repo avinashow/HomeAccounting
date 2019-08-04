8563feb6dc02ad8e4cd91794c72911f755292ad0
Pavan Mellamputi <theone.pa1@gmail.com>
2019-08-04 15:21:40 -0700

    Update CSS in transactions page

--- static/components/transactions.js
+++ static/components/transactions.js
@@ -41,7 +41,7 @@ export const TransactionPage = Vue.component('view-transactions', {
                 </div>
             </div>
         </div>
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal modal-full fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
 
